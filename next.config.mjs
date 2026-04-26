8bb0e1e9e3f86532db9a1a55ff0ffaf0f10efe44
jaredejr <jaredejr@gmail.com>
2026-04-26 19:04:55 -0300

    ajustes adicionais de metadata e redirecionamento

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,17 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        // Redireciona qualquer caminho que tenha pelo menos 1 caractere após a barra
+        // Exclui arquivos do sistema e a própria home (que não tem nada após a barra no matcher)
+        source: '/:path((?!api|_next|favicon.ico).+)',
+        destination: '/',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
