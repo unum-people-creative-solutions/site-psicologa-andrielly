cbea03357427a428c94f6c5cfb19ba0b1185e367
jaredejr <jaredejr@gmail.com>
2026-04-28 00:41:57 -0300

    ajuste de rotas

--- next.config.mjs
+++ next.config.mjs
@@ -12,8 +12,8 @@ const nextConfig = {
     return [
       {
         // Redireciona qualquer caminho que tenha pelo menos 1 caractere após a barra
-        // Exclui arquivos do sistema e a própria home (que não tem nada após a barra no matcher)
-        source: '/:path((?!api|_next|favicon.ico).+)',
+        // Exclui arquivos do sistema, SEO, imagens e a própria home
+        source: '/:path((?!api|_next|favicon.ico|sitemap.xml|robots.txt|images|icon.png).+)',
         destination: '/',
         permanent: false,
       },
